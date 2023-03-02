a5904ec1b27e1530b59bd96f0238e2ddb45202bb
piotr biedermann <piotr.biedermann84@gmail.com>
2023-03-02 15:56:40 +0100

    need error checks

--- PhoneBook.hpp
+++ PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 16:12:04 by pbiederm          #+#    #+#             */
-/*   Updated: 2023/03/02 15:01:41 by pbiederm         ###   ########.fr       */
+/*   Updated: 2023/03/02 15:20:58 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 class PhoneBook {
 	private:
 		Contact contact[8];
-		int		oldest;
+		// int		oldest;
 	public:
 		PhoneBook( void );
 		~PhoneBook( void );
 		void SetContact( Contact &contactData, int index );
 		Contact &GetContact( int index );
 		void SetOldest( int inputOldest );
-		int GetOldest (void);
+		// int GetOldest (void);
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 16:16:07 by pbiederm          #+#    #+#             */
-/*   Updated: 2023/03/02 15:12:14 by pbiederm         ###   ########.fr       */
+/*   Updated: 2023/03/02 15:55:19 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 #include "./Contact.hpp"
 #include <iostream>
 #include <iomanip>
-#include <bits/stdc++.h>
 
 #define TRUE 1
 #define SAME 0
@@ -31,27 +30,22 @@ int add(Contact &inputContact, PhoneBook &company, int index)
 	if (index > 7)
 	{
 		index = 0;
-		// index = company.GetOldest();
-		// if (index == 7)
-		// 	company.SetOldest(0);
-		// else
-		// 	company.SetOldest(index + 1);
 	}
 	std::cout << "Please enter first name: ";
 	std::cin >> inputFirstName;
 	inputContact.SetName(inputFirstName);
-	// std::cout << "Please enter last name: ";
-	// std::cin >> inputLastName;
-	// inputContact.SetLastName(inputLastName);
-	// std::cout << "Please enter nickname: ";
-	// std::cin >> inputNickname;
-	// inputContact.SetNickname(inputNickname);
-	// std::cout << "Please enter phone number: ";
-	// std::cin >> inputPhoneNumber;
-	// inputContact.SetPhoneNumber(inputPhoneNumber);
-	// std::cout << "Please enter darkest secret: ";
-	// std::cin >> inputDarkestSecret;
-	// inputContact.SetDarkestSecret(inputDarkestSecret);
+	std::cout << "Please enter last name: ";
+	std::cin >> inputLastName;
+	inputContact.SetLastName(inputLastName);
+	std::cout << "Please enter nickname: ";
+	std::cin >> inputNickname;
+	inputContact.SetNickname(inputNickname);
+	std::cout << "Please enter phone number: ";
+	std::cin >> inputPhoneNumber;
+	inputContact.SetPhoneNumber(inputPhoneNumber);
+	std::cout << "Please enter darkest secret: ";
+	std::cin >> inputDarkestSecret;
+	inputContact.SetDarkestSecret(inputDarkestSecret);
 	company.SetContact(inputContact, index);
 	index++;
 	return (index);
@@ -60,6 +54,7 @@ int add(Contact &inputContact, PhoneBook &company, int index)
 void search(PhoneBook &company, int index)
 {
 	int localIndex = 0;
+	std::cout << index;
 	while (localIndex < index)
 	{
 		std::string firstNameToPrint;
@@ -70,24 +65,23 @@ void search(PhoneBook &company, int index)
 			firstNameToPrint = company.GetContact(localIndex).GetName().substr(0, 9).append(".");
 		else
 			firstNameToPrint = company.GetContact(localIndex).GetName().substr(0, 10);
-		// if (company.GetContact(localIndex).GetLastName().length() > 10)
-		// 	lastNameToPrint = company.GetContact(localIndex).GetLastName().substr(0, 9).append(".");
-		// else
-		// 	lastNameToPrint = company.GetContact(localIndex).GetLastName().substr(0, 10);
-		// if (company.GetContact(localIndex).GetNickname().length() > 10)
-		// 	nicknameToPrint = company.GetContact(localIndex).GetNickname().substr(0, 9).append(".");
-		// else
-		// 	nicknameToPrint = company.GetContact(localIndex).GetLastName().substr(0, 10);
-		// if (company.GetContact(localIndex).GetPhoneNumber().length() > 10)
-		// 	phoneNumberToPrint = company.GetContact(localIndex).GetPhoneNumber().substr(0, 9).append(".");
-		// else
-		// 	phoneNumberToPrint = company.GetContact(localIndex).GetPhoneNumber().substr(0, 10);
+		if (company.GetContact(localIndex).GetLastName().length() > 10)
+			lastNameToPrint = company.GetContact(localIndex).GetLastName().substr(0, 9).append(".");
+		else
+			lastNameToPrint = company.GetContact(localIndex).GetLastName().substr(0, 10);
+		if (company.GetContact(localIndex).GetNickname().length() > 10)
+			nicknameToPrint = company.GetContact(localIndex).GetNickname().substr(0, 9).append(".");
+		else
+			nicknameToPrint = company.GetContact(localIndex).GetLastName().substr(0, 10);
+		if (company.GetContact(localIndex).GetPhoneNumber().length() > 10)
+			phoneNumberToPrint = company.GetContact(localIndex).GetPhoneNumber().substr(0, 9).append(".");
+		else
+			phoneNumberToPrint = company.GetContact(localIndex).GetPhoneNumber().substr(0, 10);
 		std::cout << std::setw(10) << localIndex << " | ";
 		std::cout << std::setw(10) << firstNameToPrint << " | ";
-		std::cout << std::endl;
-		// std::cout << std::setw(10) << lastNameToPrint << " | ";
-		// std::cout << std::setw(10) << nicknameToPrint.substr(0, 10) << " | ";
-		// std::cout << std::setw(10) << phoneNumberToPrint.substr(0, 10) << std::endl;
+		std::cout << std::setw(10) << lastNameToPrint << " | ";
+		std::cout << std::setw(10) << nicknameToPrint.substr(0, 10) << " | ";
+		std::cout << std::setw(10) << phoneNumberToPrint.substr(0, 10) << std::endl;
 		localIndex++;
 	}
 	std::string searchPrompt = "Enter entry id nb: ";
@@ -95,16 +89,16 @@ void search(PhoneBook &company, int index)
 	int querryIndex;
 	std::cin >> querryIndex;
 	std::string fullFistName = company.GetContact(querryIndex).GetName();
-	// std::string fullLastName = company.GetContact(querryIndex).GetLastName();
-	// std::string fullNickname = company.GetContact(querryIndex).GetNickname();
-	// std::string fullPhoneNumber = company.GetContact(querryIndex).GetPhoneNumber();
-	// std::string fullDarkSecret = company.GetContact(querryIndex).GetDarkSecret();
+	std::string fullLastName = company.GetContact(querryIndex).GetLastName();
+	std::string fullNickname = company.GetContact(querryIndex).GetNickname();
+	std::string fullPhoneNumber = company.GetContact(querryIndex).GetPhoneNumber();
+	std::string fullDarkSecret = company.GetContact(querryIndex).GetDarkSecret();
 	std::cout << "Recovering full data: " << std::endl;
 	std::cout << "name: " << fullFistName << std::endl;
-	// std::cout << "last name: " << fullLastName << std::endl;
-	// std::cout << "nick: " << fullNickname << std::endl;
-	// std::cout << "phone number: " << fullPhoneNumber << std::endl;
-	// std::cout << "dark secret: " << fullDarkSecret << std::endl;
+	std::cout << "last name: " << fullLastName << std::endl;
+	std::cout << "nick: " << fullNickname << std::endl;
+	std::cout << "phone number: " << fullPhoneNumber << std::endl;
+	std::cout << "dark secret: " << fullDarkSecret << std::endl;
 }
 
 int main()
@@ -118,10 +112,10 @@ int main()
 	std::string searchCmd = "SEARCH";
 	std::string readInput;
 	int index;
-	int oldest;
+	int number;
 
-	company.SetOldest(0);
 	index = 0;
+	number = 0;
 	std::cout << prompt;
 	while (std::cin >> readInput)
 	{
@@ -129,6 +123,8 @@ int main()
 		if (readInput.compare(addCmd) == SAME)
 		{
 			index = add(inputContact, company, index);
+			if (number < 8)
+				number++;
 		}
 		else if (readInput.compare(exitCmd) == SAME)
 		{
@@ -136,17 +132,12 @@ int main()
 		}
 		else if (readInput.compare(searchCmd) == SAME)
 		{
-			search(company, index);
+			search(company, number);
 		}
 		else
 		{
 			std::cout << "Invalid command" << std::endl;
 		}
-		// if(readInput.eof() == 1)
-		// {
-		// 	std::cout << "ctrl - D has been pressed, the phonebook will now exit" << std::endl;
-		// 	exit(0);
-		// }
 		std::cout << "enter ADD, SEARCH or EXIT" << std::endl;
 		std::cout << prompt;
 	}
